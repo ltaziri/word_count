7daa4361823aa8413033bc8669373ef08469abad
Hackbright Student <info@hackbrightacademy.com>
2016-01-11 13:08:23 -0800

    transform words to lower case and use .iteritems()

--- wordcount.py
+++ wordcount.py
@@ -6,15 +6,30 @@ def word_frequency(file_name):
     dict_word = {}
 
     for line in data_file:
+        
+        
         line = line.rstrip()
         words = line.split(" ")
-        for word in words:
+        
+        lower_words = [word.lower() for word in words]
+
+        # lower_words = []
+        # for word in words:
+        #     lower_words.append(word.lower())
+
+
+        for word in lower_words:
             dict_word[word] = dict_word.get(word, 0) + 1
 
-    print dict_word
+
+    for key, value in dict_word.iteritems():
+        print key, value
+
     return dict_word
 
     data_file.close()
 
 word_frequency("test.txt")
+# word_frequency("twain.txt")
+
 
